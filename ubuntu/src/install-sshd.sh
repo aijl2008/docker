dcc435b5480dc10f8b07c70d66214d3382fc2b40
Jerry Ai <aijl2008@qq.com>
2018-03-29 21:41:28 +0800

    Update install-sshd.sh

--- ubuntu/src/install-sshd.sh
+++ ubuntu/src/install-sshd.sh
@@ -5,4 +5,9 @@ sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_
 # SSH login fix. Otherwise user is kicked off after login
 sed 's@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g' -i /etc/pam.d/sshd
 echo "export VISIBLE=now" >> /etc/profile
+
+echo '
+[program:sshd]
+command=/usr/sbin/sshd -D
+' > /etc/supervisor/conf.d/sshd.ini
  
