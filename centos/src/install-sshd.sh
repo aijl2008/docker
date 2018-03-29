951fa5c40b0118c143aaa89b755abc6280441e87
Jerry Ai <aijl2008@qq.com>
2018-03-29 21:58:49 +0800

    Update install-sshd.sh

--- centos/src/install-sshd.sh
+++ centos/src/install-sshd.sh
@@ -3,3 +3,9 @@ set -e
 yum install -y openssh openssh-clients openssh-server
 /usr/sbin/sshd-keygen -A
 echo 'root:centos' |chpasswd
+
+echo '
+[program:sshd]
+command=/usr/sbin/sshd -D
+' > /etc/supervisor/supervisor.d/sshd.ini
+ 
