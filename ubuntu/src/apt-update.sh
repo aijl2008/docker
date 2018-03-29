c6d9bdc18136d10dd68f9f387defbf812e14b15d
Jerry Ai <aijl2008@qq.com>
2018-03-29 21:28:55 +0800

    Update apt-update.sh

--- ubuntu/src/apt-update.sh
+++ ubuntu/src/apt-update.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-    sed -i "s/archive.ubuntu.com/cn.archive.ubuntu.com/g" /etc/apt/sources.list
-    apt update
+sed -i "s/archive.ubuntu.com/cn.archive.ubuntu.com/g" /etc/apt/sources.list
+apt update
