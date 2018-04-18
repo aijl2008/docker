7e7bc998161b55ead2d92cac0fabcfc8092b14ac
Jerry Ai <awz@awz.cn>
2018-04-18 22:18:48 +0800

    up

---
+++ centos-nginx-php7-php5/src/install-php-memcache-2.2.7.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+cd /data/src 
+/usr/local/php-5.4.43/bin/pecl download memcache-2.2.7
+tar -zxvf memcache-2.2.7.tgz
+cd memcache-2.2.7
+/usr/local/php-5.4.43/bin/phpize
+./configure \
+  --with-php-config=/usr/local/php-5.4.43/bin/php-config
+make 
+make install
+echo "extension=mongodb.so" >> /usr/local/php-5.4.43/lib/php.ini
+
+
