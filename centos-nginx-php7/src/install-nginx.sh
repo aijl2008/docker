5e1acf07c82773d5d06debcd303e5080713b9226
aijl2008 <Jerry790928>
2018-04-18 18:16:42 +0800

    up

--- centos-nginx-php7/src/install-nginx.sh
+++ centos-nginx-php7/src/install-nginx.sh
@@ -44,57 +44,6 @@ make && make install
 mkdir -p /usr/local/nginx-1.12.2/conf/vhost
 mkdir -p /data/logs/nginx
 
-
-echo 'user www-data;
-worker_processes  2;
-error_log /data/logs/nginx/error.log;
-pid /data/logs/nginx/nginx.pid;
-events {
-    worker_connections  1024;
-}
-http {
-    include mime.types;
-    client_max_body_size 20m;
-    server_names_hash_bucket_size 512;
-    default_type  application/octet-stream;
-    log_format  main  \'$host $remote_addr - $remote_user [$time_local] "$request" \'
-                      \'$request_time $upstream_response_time \'
-                      \'$status $body_bytes_sent "$http_referer" \'
-                      \'"$http_user_agent" "$http_x_forwarded_for"\';
-    access_log  /data/logs/nginx/access.log  main;
-    sendfile        on;
-    keepalive_timeout  65;
-    gzip  on;
-    include vhost/*.conf;
-}' > /usr/local/nginx-1.12.2/conf/nginx.conf
-
-echo 'server {
-    listen 80;
-    server_name localhost;
-    listen 443 ssl;
-    ssl_certificate ssl/cert.pem;
-    ssl_certificate_key ssl/cert.key;
-    charset utf-8;
-    access_log  /data/logs/nginx/default.access.log  main;
-    error_log  /data/logs/nginx/default.error.log;
-    root /data/webroot;
-    autoindex on;
-    location / {
-            index index.html index.php;
-            try_files $uri $uri/ /index.php?$args;
-    }
-    location ~ .*\.(gif|jpg|jpeg|png|bmp|swf|ico|js|css)$ {
-         expires      30d;
-         access_log off;
-    }
-    location ~ \.php$ {
-        fastcgi_pass 127.0.0.1:7113;
-        fastcgi_index index.php;
-        fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
-        include fastcgi_params;
-    }
-}'  > /usr/local/nginx-1.12.2/conf/vhost/default.conf
-
 echo '[program:nginx]
 command=/usr/local/nginx-1.12.2/sbin/nginx -g "daemon off;" -c /usr/local/nginx-1.12.2/conf/nginx.conf
 ' > /etc/supervisord.d/nginx.ini
