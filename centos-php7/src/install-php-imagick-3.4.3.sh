0f81c8f5916c5cdc605b21bfa8fa7051830bc8eb
aijl2008 <Jerry790928>
2018-08-17 21:18:18 +0800

    up

--- centos-php7/src/install-php-imagick-3.4.3.sh
+++ centos-php7/src/install-php-imagick-3.4.3.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 set -e
-yum install -y ImageMagick-devel
+yum install GraphicsMagick-dev
 cd /data/src 
-/usr/local/php-7.1.13/bin/pecl download imagick-3.4.3
-tar -zxvf imagick-3.4.3.tgz
-cd imagick-3.4.3
+/usr/local/php-7.1.13/bin/pecl download gmagick-2.0.5RC1
+tar -zxvf gmagick-2.0.5RC1.tgz
+cd gmagick-2.0.5RC1
 /usr/local/php-7.1.13/bin/phpize
 ./configure \
   --with-php-config=/usr/local/php-7.1.13/bin/php-config
 make 
 make install
-echo "extension=mongodb.so" >> /usr/local/php-7.1.13/lib/php.ini
+echo "extension=gmagick.so" >> /usr/local/php-7.1.13/lib/php.ini
 
 
 cd /data/src 
-rm -rf mongodb-1.4.2
+rm -rf gmagick-2.0.5RC1
